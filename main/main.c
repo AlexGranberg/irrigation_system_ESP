b868e2382feb81426d5e545f98df9eb02765194f
AlexGranberg <alexandergranberg1983@gmail.com>
2023-10-06 11:38:32 +0200

    first commit

---
+++ main/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+
+void app_main(void){
+
+}
