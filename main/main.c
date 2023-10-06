490023543c630d8955130511b0fdb53b97c98481
AlexGranberg <alexandergranberg1983@gmail.com>
2023-10-06 13:21:21 +0200

    hmm

--- main/main.c
+++ main/main.c
@@ -5,11 +5,44 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_chip_info.h"
-#include "esp_flash.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#include "ssd1306.h"
 
-void app_main(void){
+#define I2C_MASTER_SCL_IO 26        /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 25        /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_1    /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
 
+static ssd1306_handle_t ssd1306_dev = NULL;
+
+void app_main(void)
+{
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = (gpio_num_t)I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = (gpio_num_t)I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    conf.clk_flags = I2C_SCLK_SRC_FLAG_FOR_NOMAL;
+
+    i2c_param_config(I2C_MASTER_NUM, &conf);
+    i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
+
+    ssd1306_dev = ssd1306_create(I2C_MASTER_NUM, SSD1306_I2C_ADDRESS);
+    ssd1306_refresh_gram(ssd1306_dev);
+    ssd1306_clear_screen(ssd1306_dev, 0x00);
+
+    char data_str1[20] = {0};
+    char data_str2[20] = {0};
+    char data_str3[20] = {0};
+    sprintf(data_str1, "Soil: 75%%");
+    sprintf(data_str2, "Temperature: 22c");
+    sprintf(data_str3, "Humidity: 62%%");
+    ssd1306_draw_string(ssd1306_dev, 10, 5, (const uint8_t *)data_str1, 12, 1);
+    ssd1306_draw_string(ssd1306_dev, 10, 25, (const uint8_t *)data_str2, 12, 1);
+    ssd1306_draw_string(ssd1306_dev, 10, 45, (const uint8_t *)data_str3, 12, 1);
+    ssd1306_refresh_gram(ssd1306_dev);
 }
