3ec37549c2a99a0e158923e9515e0beaf62aa639
AlexGranberg <alexandergranberg1983@gmail.com>
2023-10-20 12:06:39 +0200

    catch2 verkar funka

--- main/ultra_test.c
+++ main/ultra_test.c
@@ -1,4 +1,4 @@
-#include "vafantest.h"
+#include "ultra_test.h"
 
 // Function definition
 int add(int a, int b) {
--- main/ultrasonic_unit_test/vafantest.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch_all.hpp>
-#include "../include/vafantest.h"
-
-TEST_CASE("Our First Test", "[Test]"){
-
-    REQUIRE(1 == 2);
-}
